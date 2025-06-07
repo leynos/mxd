fdc08b105a11c031862a7c4ced23a7ea35ca50ba
Leynos <leynos@troubledskies.net>
2025-06-07 16:02:47 +0100

    Support news bundles hierarchy (#35)
    * feat(news): add bundle hierarchy
    
    * Rename resolve_bundle_path to bundle_id_from_path

--- src/models.rs
+++ src/models.rs
@@ -19,10 +19,26 @@ pub struct NewUser<'a> {
 pub struct Category {
     pub id: i32,
     pub name: String,
+    pub bundle_id: Option<i32>,
 }
 
 #[derive(Insertable, Deserialize)]
 #[diesel(table_name = crate::schema::news_categories)]
 pub struct NewCategory<'a> {
+    pub name: &'a str,
+    pub bundle_id: Option<i32>,
+}
+
+#[derive(Queryable, Serialize, Deserialize, Debug)]
+pub struct Bundle {
+    pub id: i32,
+    pub parent_bundle_id: Option<i32>,
+    pub name: String,
+}
+
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name = crate::schema::news_bundles)]
+pub struct NewBundle<'a> {
+    pub parent_bundle_id: Option<i32>,
     pub name: &'a str,
 }
