4bf923d82465e87781d4ea4ebbcb2972b2f430c6
Leynos <leynos@troubledskies.net>
2025-06-13 07:49:04 +0100

    Add temp-env for scoped env var test

--- tests/postgres_env.rs
+++ tests/postgres_env.rs
@@ -1,24 +1,16 @@
 #[cfg(feature = "postgres")]
+use temp_env::with_var;
+#[cfg(feature = "postgres")]
 use test_util::setup_postgres_for_test;
 
 #[cfg(feature = "postgres")]
 #[test]
-    set_var("POSTGRES_TEST_URL", "postgres://example");
-    remove_var("POSTGRES_TEST_URL");
-
-#[cfg(feature = "postgres")]
-fn set_var(key: &str, value: &str) {
-    unsafe { std::env::set_var(key, value) }
-}
-
-#[cfg(feature = "postgres")]
-fn remove_var(key: &str) {
-    unsafe { std::env::remove_var(key) }
-}
-/// ```
-/// // This test is intended to be run with the "postgres" feature enabled.
-/// external_postgres_is_used().unwrap();
-/// ```
+    with_var("POSTGRES_TEST_URL", Some("postgres://example"), || {
+        let (url, pg) = setup_postgres_for_test(|_| Ok(()))?;
+        assert_eq!(url, "postgres://example");
+        assert!(pg.is_none());
+        Ok::<_, Box<dyn std::error::Error>>(())
+    })
 fn external_postgres_is_used() -> Result<(), Box<dyn std::error::Error>> {
     unsafe {
         std::env::set_var("POSTGRES_TEST_URL", "postgres://example");
