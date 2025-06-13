36d2e1ac85092835db4e425461b66ec8da8b0821
Leynos <leynos@troubledskies.net>
2025-06-13 12:35:13 +0100

    Fix Postgres test env setup (#122)

--- tests/postgres_env.rs
+++ tests/postgres_env.rs
@@ -5,21 +5,11 @@ use test_util::setup_postgres_for_test;
 
 #[cfg(feature = "postgres")]
 #[test]
+fn external_postgres_is_used() -> Result<(), Box<dyn std::error::Error>> {
     with_var("POSTGRES_TEST_URL", Some("postgres://example"), || {
         let (url, pg) = setup_postgres_for_test(|_| Ok(()))?;
         assert_eq!(url, "postgres://example");
         assert!(pg.is_none());
         Ok::<_, Box<dyn std::error::Error>>(())
     })
-fn external_postgres_is_used() -> Result<(), Box<dyn std::error::Error>> {
-    unsafe {
-        std::env::set_var("POSTGRES_TEST_URL", "postgres://example");
-    }
-    let (url, pg) = setup_postgres_for_test(|_| Ok(()))?;
-    assert_eq!(url, "postgres://example");
-    assert!(pg.is_none());
-    unsafe {
-        std::env::remove_var("POSTGRES_TEST_URL");
-    }
-    Ok(())
 }
